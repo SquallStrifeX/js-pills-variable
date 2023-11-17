e216f668d3a6c79f98ac9f0198ae827a9ab02f54
SquallStrifeX <squallstrifex@gmail.com>
2023-11-17 13:05:39 +0100

    ok

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// Esercizio 1 //
 let number = 100;
 console.log(number);
 
+// Esercizio 2 //
+const PI = 3.14;
+console.log(PI);
+
+// Esercizio 2/B //
+
+// PI =  3.1416
+// console.log(PI)
+
+// Esercizio 2/C //
+
+let radius = 8;
+
+let circle = radius * 2 * PI
+
+console.log(circle)
+
+// Esercizio 3 //
+
+let name = carlo
+
+name = marco
