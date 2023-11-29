14fe0c0a0e2d120edea1c027ad574e59f7d83465
SquallStrifeX <squallstrifex@gmail.com>
2023-11-29 10:39:19 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,80 +1,195 @@
-// Esercizio 1 //
-let number = 100;
-console.log(number);
+// // Esercizio 1 //
+// let number = 100;
+// console.log(number);
 
-// Esercizio 2 //
-const PI = 3.14;
-console.log(PI);
+// // Esercizio 2 //
+// const PI = 3.14;
+// console.log(PI);
 
-// Esercizio 2/B //
+// // Esercizio 2/B //
 
-// PI =  3.1416
-// console.log(PI)
+// // PI =  3.1416
+// // console.log(PI)
 
-// Esercizio 2/C //
+// // Esercizio 2/C //
 
-let radius = 8;
+// let radius = 8;
 
-let circle = radius * 2 * PI
+// let circle = radius * 2 * PI
 
-console.log(circle)
+// console.log(circle)
 
-// Esercizio 3 //
+// // Esercizio 3 //
 
-let name = carlo
-console.log(name)
-
-name = marco
-console.log(name)
-
-// Esercizio 4
-
-let greet = "Ciao, Mondo!"
-console.log(name)
-
-// Esercizio 4/B
-
-// let greet = "Come stai, Mondo?"
+// let name = carlo
 // console.log(name)
 
-// Esercizio 4/C
-let greet2 = "Come stai?"
-console.log(greet2)
+// name = marco
+// console.log(name)
 
-greet = greet+greet2
- console.log(greet)
+// // Esercizio 4
 
-//  Esercizio 1
-let num1 = 10
+// let greet = "Ciao, Mondo!"
+// console.log(name)
 
-let num2 = 20
+// // Esercizio 4/B
 
-let comparison
+// // let greet = "Come stai, Mondo?"
+// // console.log(name)
 
-if(num1 > num2){
-    comparison = num1
+// // Esercizio 4/C
+// let greet2 = "Come stai?"
+// console.log(greet2)
 
+// greet = greet+greet2
+//  console.log(greet)
+
+// //  Esercizio 1
+// let num1 = 10
+
+// let num2 = 20
+
+// let comparison
+
+// if(num1 > num2){
+//     comparison = num1
+
+// }
+
+// else{
+//     comparison = num 2
+// }
+
+// console.log(comparison)
+
+
+// // Esercizio 2
+
+// let paolo = 40
+
+// let carlo = 30
+
+// if(paolo == carlo) {
+//     comparison =  "le stringhe sono uguali"
+// }
+
+// else{
+//     comparison = "le stringhe sono diverse"
+// }
+
+
+// // Esercizio 3
+
+// let number = Number(15)
+
+// let string = "15"
+
+// if(number === string) {
+//     comparison =  "le stringhe sono uguali"
+// }
+
+// else{
+//     comparison = "le stringhe sono diverse"
+// }
+
+// // Esercizio 1/A
+
+//  for (let i=1; i<=42; i++){
+//     console.log(i)
+//  }
+
+// //  Esercizio 2/B
+
+// for (let i=42; i>=1; i--){
+//     console.log(i)
+// }
+
+// // Esercizio 1/A
+
+// let array = [1,2,3,4,5,6,7,8]
+
+// let fruits = [mela, banana, pesca, pera, albicocca, mango, kiwi, papaya]
+// console.log(fruits.length)
+
+// let num1 = prompt("Insierisci un numero");
+
+// let num2 = prompt("Inserisci un altro numero");
+
+// if (num1 > num2) {
+//     console.log(num1);
+// }
+
+// else {
+//     console.log(num2)
+// }
+
+//     let somma = 0
+// for (i=1; i<11; i++){
+//     let numero = parseInt(prompt("Inserisci un numero"));
+//      somma += numero
+//     console.log(somma)
+// // }
+
+// let invitati = ["gigi", "diego", "saro", "onofrio", "bardazzo"]
+
+// let nome = prompt("inserisci il tuo nome");
+
+// for (i=0; i==invitati.length; i++){
+
+// // }
+
+// let array = ""
+// let number = parseInt(prompt("Inserisci un numero"));
+
+// for (i=1; i<7; i++){
+
+//     if(number%2 == 0){
+//         array.push = (number);
+//         console.log(array)
+//     }
+
+
+// }
+
+
+// let num = prompt("Inserisci un numero");
+// let a = 0
+// let b = 1
+// let c
+
+// i=0
+// while(i<num){
+//     c= a + b;
+//     a = b;
+//     b = c; 
+//     console.log(c)
+//     i++;
+   
+
+// }
+
+
+// function helloworld(){
+//     alert("HEllo world")
+// }
+
+// helloworld()
+
+
+// function somma(num1, num2){
+//     let num1 = 5;
+//     let num2 = 6;
+//     return num1 + num2;
+
+// }
+// let num1 = 109
+// let num2 = 999
+// let sommax = somma(num1, num2);
+// console.log(sommax)
+
+
+let getOneStringFromTwo (crash, bandicoot){
+    let crash = "crash";
+    let bandicoot = "bandicoot"
+    return crash + "" + bandicoot
 }
-
-else{
-    comparison = num 2
-}
-
-console.log(comparison)
-
-
-// Esercizio 2
-
-let paolo = 40
-
-let carlo = 30
-
-if(paolo == carlo) {
-    comparison =  "le stringhe sono uguali"
-}
-
-else{
-    comparison = "le stringhe sono diverse"
-}
-
-
